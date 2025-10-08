92c191757e9d5ff7c354dd311a10cdb6b220ac26
hiclassic <hares.ahmed.319@gmail.com>
2025-10-08 12:46:52 +0600

    mcrf added

---
+++ example-app/app/Http/Controllers/HarespostController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Harespost;
+use Illuminate\Http\Request;
+
+class HarespostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Harespost $harespost)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Harespost $harespost)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Harespost $harespost)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Harespost $harespost)
+    {
+        //
+    }
+}
