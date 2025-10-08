e656f1427fa0d6f07ef729d6577bd82a44830f64
hiclassic <hares.ahmed.319@gmail.com>
2025-10-08 12:48:12 +0600

    mcrf added

--- example-app/database/migrations/2025_10_08_063810_create_haresposts_table.php
+++ example-app/database/migrations/2025_10_08_063810_create_haresposts_table.php
@@ -12,9 +12,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('haresposts', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
+    $table->id();
+    $table->string('title');
+    $table->text('content');
+    $table->string('author')->nullable();
+    $table->timestamps();
+});
+
     }
 
     /**
